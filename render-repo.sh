32c2c650e948ba9f7d19fe0d2b021195b0b9a225
iTitou <titouanchristophe@gmail.com>
2013-10-24 19:50:23 +0200

    [fix] git issues

--- render-repo.sh
+++ render-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-OKFILE = 'autopdf.done'
+OKFILE='autopdf.done'
 
 if [ ! -d $1 ]; then
 	echo " * Unknow repo. Cloning..."
@@ -8,7 +8,8 @@ if [ ! -d $1 ]; then
 fi
 
 cd $1
-git pull origin master || exit 1
+git checkout -b $3
+git pull --no-edit origin $3
 echo " * Repo pulled"
 
 echo " * Beginning pdf rendering..."
@@ -16,14 +17,12 @@ make -f Makepdf
 echo " * pdf rendering finished..."
 
 if [ -f $OKFILE ]; then
-	git checkout -b $3
 	git add $(head $OKFILE)
 	echo " * Changes added"
 	git commit -m "[autopdf] PDF rendering ($(egrep -h "$(date +"%m/%d|%b* %d")" /usr/share/calendar/* | head -1))"
 	rm -f $OKFILE
 	echo " * Changes commited"
 	git push origin $3 || exit 1
-	git branch -D $3
 	echo " * Changes pushed"
 else
 	echo " ! No autopdf.done file found"
