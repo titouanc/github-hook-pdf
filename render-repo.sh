3eb9f3bfd6c64959f08a065d8d3982c8457aa9f0
iTitou <titouanchristophe@gmail.com>
2013-10-24 19:40:45 +0200

    [UNSTABLE] Attempt to fix issues

--- render-repo.sh
+++ render-repo.sh
@@ -16,11 +16,14 @@ make -f Makepdf
 echo " * pdf rendering finished..."
 
 if [ -f $OKFILE ]; then
-	git add $(cat $OKFILE)
+	git checkout -b $3
+	git add $(head $OKFILE)
+	echo " * Changes added"
 	git commit -m "[autopdf] PDF rendering ($(egrep -h "$(date +"%m/%d|%b* %d")" /usr/share/calendar/* | head -1))"
 	rm -f $OKFILE
 	echo " * Changes commited"
-	git push origin master:$3 || exit 1
+	git push origin $3 || exit 1
+	git branch -D $3
 	echo " * Changes pushed"
 else
 	echo " ! No autopdf.done file found"
