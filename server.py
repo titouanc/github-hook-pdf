acec0ccf5513e320f13e5ba540d2e0fc0c236e40
iTitou <moiandme@gmail.com>
2013-10-24 22:06:24 +0200

    [ref] detect strangers as soon as possible plus rebuild if Makepdf changed

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import json
 import subprocess
 
-known_repos = ['my-awesome-repo']
+known_repos = ['Stage-Railnova']
 branch_name = "autopdf"
 
 def do_pdf(repo):
@@ -17,12 +17,14 @@ webapp = Flask(__name__)
 def postReceiveHook():
 	data = json.loads(request.form['payload'])
 	changes_tex = []
+	if data['repository']['name'] not in known_repos:
+		return "I don't like you. Goodbye"
 	if data['ref'].split('/')[-1] == 'master':
 		for commit in data['commits']:
 			for file_changed in commit['added'] + commit['modified']:
-				if file_changed[-4:] == '.tex':
+				if file_changed[-4:] == '.tex' or file_changed == 'Makepdf':
 					changes_tex.append(file_changed)
-		if len(changes_tex)>0 and data['repository']['name'] in known_repos:
+		if len(changes_tex)>0:
 			do_pdf(data['repository'])
 	return "Thx"
 
